0884aeafdfa9de82a0ac6a12a714b2de7a7f85c4
flatgreen <flatgreen@gmail.com>
2016-10-29 15:42:43 +0200

    maj arte

---
+++ build/lib/arte_court_circuit/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+    Ce script DL la page
+    http://cinema.arte.tv/fr/magazine/court-circuit/emissions
+    'fr' uniquement (mais pas très difficile de faire en 'de')
+
+    trouve la page de la dernière emission téléchargeable
+    Sur la page : liste les "métrage" et les DL
+
+    vers le début fev maj de ytdl avec arte cinema (à suivre)
+
+    m.a.j : 15 oct 2016
+"""
+
+__version__ = "0.1.0"
